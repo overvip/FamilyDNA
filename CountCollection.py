a436b8089f00e701026fd4dfd6b5b91615dd83f9
overvip <huangx2006@gmail.com>
2019-02-18 23:12:26 +0800

    控制台按村显示人和家系总数及完成数

--- CountCollection.py
+++ CountCollection.py
@@ -1,17 +1,19 @@
 # -*- coding:utf-8 -*-
 import openpyxl
 
-villages = ('鼎美村',)  #, '后柯村', '芸美村')
-wb = openpyxl.load_workbook('C:\\Users\shine\\OneDrive\\社区采血名单.xlsx')
+f='C:\\Users\\shine\\\onedrive\\社区采血名单.xlsx'
+villages = ('鼎美村', '后柯村', '芸美村')
+wb = openpyxl.load_workbook(f)
 for village in villages:
     tot_families = coll_families = tot_persons = coll_persons = 0
-    sheetHK = wb[village]
-    # sheetHK = wb.get_active_sheet()
-    # print(sheetHK['g15'].value==None)
-    # print(sheetHK['a3'].row)
+    wsVillage = wb[village]
+    # print(wsVillage.merged_cells)
+    # wsVillage = wb.get_active_sheet()
+    # print(wsVillage['g15'].value==None)
+    # print(wsVillage['a3'].row)
     prev_fam_complete = 0
     family_name = None
-    for row in sheetHK.rows:
+    for row in wsVillage.rows:
         if row[0].row > 2:
             if row[1].value is not None:
                 tot_families += 1
@@ -31,4 +33,11 @@ for village in villages:
     if prev_fam_complete == 1:
         coll_families += 1
         print(family_name)
-    print(village + '家系数' + str(coll_families) + '个')
+    msgResult = village + '家系总数' + str(tot_families) + '个,已采集' + \
+        str(coll_families) + '个，采集总数' + str(tot_persons) + \
+        '个，已采集' + str(coll_persons) + '个'
+    print(msgResult)
+    input('按回车显示下一个村')
+    wsVillage['a1'].value = msgResult  # village + '家系数' + str(coll_families) + '个'
+    wb.save(f)
+
